a854d9929540ec741e87f4fa0c5f3539bd6b782f
rasyid2027 <halimurrasyid56@gmail.com>
2020-07-11 08:36:39 +0700

    delete some useless file

--- auth-login.php
+++ auth-login.php
@@ -1,15 +1,44 @@
+<?php 
+
+if( isset($_POST['login']) )
+{
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $result = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'");
+
+  if( mysqli_num_rows($result) === 1 )
+  {
+    
+    $row = mysqli_fetch_assoc($result);
+    if( password_verify($password, $row['password']) )
+    {
+
+        header('Location: index.php');
+        exit;
+    }
+  }
+
+  $error = true;
+  
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>Subscribe &mdash; Stisla</title>
+  <title>Login &mdash; Stisla</title>
 
   <!-- General CSS Files -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
   <!-- CSS Libraries -->
+  <link rel="stylesheet" href="../node_modules/bootstrap-social/bootstrap-social.css">
 
   <!-- Template CSS -->
   <link rel="stylesheet" href="../assets/css/style.css">
@@ -21,37 +50,52 @@
     <section class="section">
       <div class="container mt-5">
         <div class="row">
-          <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-6 offset-xl-3">
+          <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              Stisla
+              <img src="../assets/img/mp logorostchild.png" alt="logo" width="110" class="shadow-light rounded-circle">
             </div>
 
             <div class="card card-primary">
-              <div class="card-header"><h4>Subscribe Our Newsletters</h4></div>
+              <div class="card-header"><h4>Login</h4></div>
 
               <div class="card-body">
-                <form method="POST">
+                <form method="POST" action="#" class="needs-validation" novalidate="">
                   <div class="form-group">
-                    <div class="input-group">
-                      <div class="input-group-prepend">
-                        <div class="input-group-text">
-                          <i class="fas fa-envelope"></i>
-                        </div>
-                      </div>
-                      <input id="email" type="email" class="form-control" name="email" autofocus placeholder="Email">
+                    <label for="username">Username</label>
+                    <input id="username" type="username" class="form-control" name="username" tabindex="1" required autofocus>
+                    <div class="invalid-feedback">
+                      Please fill in your username
                     </div>
                   </div>
 
-                  <div class="form-group text-center">
-                    <button type="submit" class="btn btn-lg btn-round btn-primary">
-                      Subscribe
+                  <div class="form-group">
+                    <div class="d-block">
+                      <label for="password" class="control-label">Password</label>
+                    </div>
+                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
+                    <div class="invalid-feedback">
+                      please fill in your password
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="custom-control custom-checkbox">
+                      <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
+                      <label class="custom-control-label" for="remember-me">Remember Me</label>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" name="login" tabindex="4">
+                      Login
                     </button>
                   </div>
                 </form>
+
               </div>
             </div>
             <div class="simple-footer">
-              Copyright &copy; Stisla 2018
+              Copyright &copy; Halimurrasyid 2020
             </div>
           </div>
         </div>
